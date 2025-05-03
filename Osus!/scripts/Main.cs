7e280a4202db71411f14db3f140aefdcf4b50a6c
vividskies <dannyc52521@gmail.com>
2025-05-04 03:05:54 +1000

    convert to .NET

---
+++ Osus!/scripts/Main.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+public partial class Main : Node
+{
+		public override void _Ready()
+		{
+				// Called every time the node is added to the scene.
+				// Initialization here
+				GD.Print("Hello, World!");
+		}
+
+}
